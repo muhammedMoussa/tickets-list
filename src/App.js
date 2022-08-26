0d9c2128ce130bdade38b2ccc918421da9c03755
Muhammed Moussa <muhammedmoussa@hotmail.com>
2022-08-26 15:59:02 +0200

    refactor

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
-import logo from './logo.svg'
 import './App.css'
-import { useState } from 'react'
 import { Item } from './components/Item'
 import VirtualScroll from './components/VirtualScroll'
 
